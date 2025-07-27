0e2afec3fecc34bb91e9dd25d370a77c9992400f
MindStudioOfficial <marc@bachrc.de>
2025-07-27 22:22:07 +0200

    Initial CMake Project Setup

---
+++ run_debug.bat
@@ -0,0 +1,3 @@
+cmake --build build --parallel --config Debug
+
+call .\build\Debug\scopes_plusplus.exe
---
+++ src/app/src/main.cpp
@@ -0,0 +1,11 @@
+#include "imgui.h"
+#include "backends/imgui_impl_glfw.h"
+#include "backends/imgui_impl_opengl3.h"
+
+#include <print>
+
+int main() {
+    std::println("Hello, Scopes++!\n");
+
+    return EXIT_SUCCESS;
+}
